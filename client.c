774e8b1c49aeba473fe7accbe5156e5d3cbfadf0
Léo <leogaudinoff@gmail.com>
2023-06-09 17:24:20 +0200

    feat: implement mandatory part

---
+++ client.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/09 15:02:14 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/06/09 16:53:18 by lgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * @brief    Sends 8 signals to the provided PID to transmit the
+ * provided character bit by bit.
+ * It starts from the MSB and progressively goes to the LSB.
+ *
+ * It sends SIGUSR1 if the bit is 1, SIGUSR2 if it is 0.
+ *
+ * @param    pid       server's PID
+ * @param    character character to transmit
+ */
+void	send_signal(int pid, unsigned char character)
+{
+	int				i;
+	unsigned char	temp_char;
+
+	i = 8;
+	temp_char = character;
+	while (i > 0)
+	{
+		i--;
+		temp_char = character >> i;
+		if (temp_char % 2 == 0)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		usleep(42);
+	}
+}
+
+/**
+ * @brief    Sends a message to the server character by character.
+ *
+ * @param    argc
+ * @param    argv
+ */
+int	main(int argc, char *argv[])
+{
+	pid_t		server_pid;
+	const char	*message;
+	int			i;
+
+	if (argc != 3)
+	{
+		ft_printf("Usage: %s <pid> <message>\n", argv[0]);
+		exit(0);
+	}
+	server_pid = ft_atoi(argv[1]);
+	message = argv[2];
+	i = 0;
+	while (message[i])
+		send_signal(server_pid, message[i++]);
+	return (0);
+}
---
+++ server.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/09 15:02:13 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/06/09 16:49:47 by lgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define END_TRANSMISSION '\0'
+
+/**
+ * @brief    Checks if the signal is SIGUSR1. If it is, it will
+ * assign 1 to the LSB. Else, it will assign 0 (actually it simply
+ * won't modify it).
+ *
+ * Example:
+ * 00101100   current_char
+ * 00000001   result of (sigsent == SIGUSR1)
+ * --------
+ * 00101101   result stored in message after the bitwise OR operation
+ *
+ * It will then increment the bit index.
+ * If it is 8, it means that
+ * the char has been fully transmitted. It will then print it and
+ * reset the bit index and the current char.
+ * Else, it will shift the current char to the left by 1.
+ *
+ * @param    signal    SIGUSR1 or SIGUSR2
+ */
+void	handle_signal(int signal)
+{
+	static unsigned char current_char;
+	static int bit_index;
+
+	current_char |= (signal == SIGUSR1);
+	bit_index++;
+	if (bit_index == 8)
+	{
+		ft_printf("%c", current_char);
+		bit_index = 0;
+		current_char = 0;
+	}
+	else
+		current_char <<= 1;
+}
+
+/**
+ * @brief    Prints its program's PID and calls the signal handlers.
+ */
+int	main(void)
+{
+	printf("%d\n", getpid());
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
+	while (1)
+		pause();
+	return (0);
+}
